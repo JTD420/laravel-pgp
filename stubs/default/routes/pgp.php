f901ffb56051017ce960fd3140b311c824c642c2
JTD420 <jtd420@users.noreply.github.com>
2023-02-09 20:43:16 +0000

    Added Routes/pgp.php and moved all PGP related routes into it.
    Signed-off-by: Brad <contact-me@h4xr0x.com>

---
+++ stubs/default/routes/pgp.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\PGPController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Laravel-PGP Routes
+|--------------------------------------------------------------------------
+|
+| This file includes routes for the Laravel-PGP package, offering
+| PGP encryption/decryption in Laravel for secure communication.
+| Customizable chat interface included.
+|
+*/
+
+$prefix = config('PGP.prefix');
+Route::prefix("$prefix")->group(function () {
+    Route::get('/message', [PGPController::class, 'index'])->name('messages.index');
+    Route::get('/PGP', function () {
+        return view('PGP::welcome');
+    });
+
+});
