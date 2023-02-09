99148c7b60d0c5df744d8e78de5e5958d4cb81fd
JTD420 <jtd420@users.noreply.github.com>
2023-02-09 01:16:18 +0000

    Adds classmap to composer.json when package is installed
    Signed-off-by: Brad <contact-me@h4xr0x.com>

---
+++ src/add-classmap.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Adds the necessary classmap to applications composer.json for PGP to work with Laravel.
+ *
+ * @return void
+ */
+
+//$basePath = realpath(__DIR__ . '/../../../');
+//ToDo: Remove hardcoded path prior to releasing composer package!
+$basePath = 'C:/xampp/htdocs/laravel-pgp-app/';
+
+$composerJsonFile = $basePath . 'composer.json';
+
+$composerJson = json_decode(file_get_contents($composerJsonFile), true);
+
+$composerJson['autoload']['classmap'] = ["vendor/singpolyma/openpgp-php/lib/"];
+
+file_put_contents($composerJsonFile, json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+
