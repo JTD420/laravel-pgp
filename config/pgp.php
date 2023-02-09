b5bfdd0368835cfa12538433b1c3baee5eb232d9
JTD420 <jtd420@users.noreply.github.com>
2023-02-09 20:41:54 +0000

    Updated Default Config
    Signed-off-by: Brad <contact-me@h4xr0x.com>

--- config/pgp.php
+++ config/pgp.php
@@ -2,5 +2,13 @@
 
 // config for JTD420/PGP
 return [
+    /*
+     * To prevent naming conflicts, this prefix will be added to all Laravel-PGP migrations.
+     */
+    'table_prefix' => 'pgp',
 
+    /*
+     * Prefix added to rest of Laravel-PGP code, including routes. (Delete default to remove prefix)
+     */
+    'prefix' => 'PGP',
 ];
