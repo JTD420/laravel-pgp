98a01592f03f5817b390170d9dee1d8c24b639b1
JTD420 <jtd420@users.noreply.github.com>
2023-02-09 20:39:03 +0000

    Added ModelTrait.php to prepend the Table_Prefix from config file onto models table name when set.
    Signed-off-by: Brad <contact-me@h4xr0x.com>

---
+++ src/Console/ModelTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace JTD420\PGP\Console;
+
+use Illuminate\Support\Str;
+
+/**
+ * Credits to : https://stackoverflow.com/a/57337748
+ */
+trait ModelTrait
+{
+    /**
+     * Scopped Variables
+     */
+    protected $table_prefix;
+
+    /**
+     * Appends prefix to table name
+     *
+     * @return $table
+     */
+    public function getTable()
+    {
+
+        $this->table_prefix = config('PGP.table_prefix');
+
+        $model = explode("\\", get_class($this));
+        $model = Str::lower(array_pop($model));
+
+        if (!isset($this->table)) $this->setTable(Str::plural($this->table_prefix . $model));
+
+        return $this->table;
+    }
+}
