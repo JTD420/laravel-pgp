3ed29cb5fefe92a26cd98eb1f401c9b69d1e6f52
JTD420 <jtd420@users.noreply.github.com>
2023-02-15 23:41:23 +0000

    updated PGPServiceProvider.php
    Signed-off-by: Brad <contact-me@h4xr0x.com>

--- src/PGPServiceProvider.php
+++ src/PGPServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace JTD420\PGP;
 
+use Illuminate\Support\Facades\Artisan;
+use JTD420\PGP\Commands\AddUserModelChanges;
+use JTD420\PGP\Commands\AddUserNameColumn;
 use JTD420\PGP\Commands\InstallCommand;
 
 class PGPServiceProvider extends PackageServiceProvider
@@ -14,17 +17,34 @@ class PGPServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('PGP')
+            ->name('laravel-PGP')
+            ->publishesServiceProvider('PGPAppServiceProvider')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_pgp_keys_table')
-            ->hasMigration('create_pgp_messages_table')
+            ->hasMigrations(
+                'create_pgp_keys_table',
+                'create_pgp_conversations_table',
+                'create_pgp_message_recipients_table',
+                'create_pgp_encrypted_messages_table',
+                'create_pgp_replies_table',
+                'create_pgp_reply_recipients_table',
+            //'create_pgp_replies_table',
+            )
+            ->hasCommands(AddUserModelChanges::class, AddUserNameColumn::class)
             ->hasInstallCommand(function (InstallCommand $command) {
                 $command
+                    ->startWith(function (InstallCommand $command) {
+                        $command->call('add-user-model-changes');
+                        $command->call('add-username-column');
+                        Artisan::call('migrate', ['--path' => 'database/migrations/2014_10_12_000000_create_users_table.php']);
+                        Artisan::call('migrate', ['--path' => 'database/migrations/add_username_to_users_table.php']);
+                    })
                     ->publishConfigFile()
                     ->publishMigrations()
                     ->askToRunMigrations()
+                    ->askToPublishViewFiles()
                     ->copyAndRegisterServiceProviderInApp()
+                    ->copyAndRegisterGravatarProviderInApp()
                     ->askToStarRepoOnGitHub('JTD420/Laravel-PGP');
             });
     }
